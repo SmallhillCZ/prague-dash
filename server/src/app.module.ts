5c28394ae82f07362407ab6103571b6274d9d828
Martin Kopeček <martin.kopecek@smallhill.cz>
2021-10-12 15:33:26 +0200

    remove appservice imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { ContainersModule } from './modules/containers/containers.module';
 import { PublicTransportModule } from './modules/public-transport/public-transport.module';
