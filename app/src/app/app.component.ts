8512413aa14992da46be339a79882d3384ae7af5
Martin Kopeček <martin.kopecek@smallhill.cz>
2021-10-13 19:22:53 +0200

    add splash screen hide and back button quit

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { App } from '@capacitor/app';
+import { IonRouterOutlet, Platform, ViewDidEnter } from '@ionic/angular';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements ViewDidEnter {
   title = 'prague-dash';
+
+  @ViewChild(IonRouterOutlet, { static: true }) routerOutlet!: IonRouterOutlet;
+
+  constructor(
+    private platform: Platform,
+  ) {
+
+    // Quit on back button when on root page
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        App.exitApp();
+      }
+    });
+  }
+
+  ionViewDidEnter() {
+    SplashScreen.hide();
+  }
+
+
 }
