ac41679b419ca692351fec22dc5eb35db6a385d0
Martin Kopeček <martin.kopecek@smallhill.cz>
2022-01-06 19:16:15 +0100

    patch: download only real stops

--- server/src/modules/public-transport/services/stops-download.service.ts
+++ server/src/modules/public-transport/services/stops-download.service.ts
@@ -41,8 +41,16 @@ export class StopsDownloadService {
       offset += 10000;
 
       data.features
-        .filter(item => !!item.properties.stop_name)
         .forEach(item => {
+
+          // • 0 (or empty): Stop (or Platform). A location where passengers board or disembark from a transit vehicle. Is called a platform when defined within a parent_station.
+          // • 1: Station. A physical structure or area that contains one or more platform.
+          // • 2: Entrance/Exit. A location where passengers can enter or exit a station from the street. If an entrance/exit belongs to multiple stations, it can be linked by pathways to both, but the data provider must pick one of them as parent.
+          // • 3: Generic Node. A location within a station, not matching any other location_type, which can be used to link together pathways define in pathways.txt.
+          // • 4: Boarding Area. A specific location on a platform, where passengers can board and/or alight vehicles.
+          if (item.properties.location_type) return;
+
+          if (!item.properties.stop_name) return;
 
           if (!stopIndex[item.properties.stop_name]) {
             const stop = new Stop();
